c00b3760f157e58a9f9c57c15ff66bf8cd0ec6cf
Christian <hc.dolling@mail.de>
2020-04-30 15:00:51 +0200

    Fixed Weapon Position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -92,7 +92,7 @@ public class PlayerController : MonoBehaviour
 
 
             // Instantiate the new Weapon
-            equippedWeapon = Instantiate(newWeapon.gameObject, weaponSlot.position, weaponSlot.rotation, transform);
+            equippedWeapon = Instantiate(newWeapon.gameObject, weaponSlot.position, weaponSlot.rotation, weaponSlot.transform);
 
             // Return old Weapon back to Collectable
             return oldWeapon;
